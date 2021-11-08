d96b74c779993795c913ff7130e898e0bf06426e
IYanakiev34 <ivan.y.yanakiev@gmail.com>
2021-11-08 02:59:02 +0100

    "player movemen back on track"

--- src/Game.cpp
+++ src/Game.cpp
@@ -32,12 +32,6 @@ void Game::processEvents()
 	{
 		switch (event.type)
 		{
-			/*case sf::Event::KeyPressed:
-				handlePlayerInput(event.key.code, true);
-				break;
-			case sf::Event::KeyReleased:
-				handlePlayerInput(event.key.code, false);
-				break;*/
 			case sf::Event::Closed:
 				m_window.close();
 				break;
@@ -46,11 +40,6 @@ void Game::processEvents()
 		}
 	}
 }
-
-/*void Game::handlePlayerInput(sf::Keyboard::Key key, bool state)
-{
-
-}*/
 
 void Game::update(sf::Time deltaTime)
 {
--- src/World.cpp
+++ src/World.cpp
@@ -23,6 +23,25 @@ void World::update(sf::Time dt)
 	// Scroll the world
 	mWorldView.move(0.f, mScrollSpeed * dt.asSeconds());
 
+	sf::Vector2f movement(0.f, 0.f);
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		movement.y -= 150.f;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		movement.y += 150.f;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		movement.x -= 150.f;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		movement.x += 150.f;
+	}
+
+	mPlayerAircraft->move(movement * dt.asSeconds());
 	// Move the player sidewards (plane scouts follow the main aircraft)
 	sf::Vector2f position = mPlayerAircraft->getPosition();
 	sf::Vector2f velocity = mPlayerAircraft->getVelocity();
@@ -77,15 +96,5 @@ void World::buildScene()
 	std::unique_ptr<Aircraft> leader(new Aircraft(Aircraft::Eagle, mTextures));
 	mPlayerAircraft = leader.get();
 	mPlayerAircraft->setPosition(mSpawnPosition);
-	mPlayerAircraft->setVelocity(40.f, mScrollSpeed);
 	mSceneLayers[Air]->attachChild(std::move(leader));
-
-	// Add two escorting aircrafts, placed relatively to the main plane
-	std::unique_ptr<Aircraft> leftEscort(new Aircraft(Aircraft::Raptor, mTextures));
-	leftEscort->setPosition(-80.f, 50.f);
-	mPlayerAircraft->attachChild(std::move(leftEscort));
-
-	std::unique_ptr<Aircraft> rightEscort(new Aircraft(Aircraft::Raptor, mTextures));
-	rightEscort->setPosition(80.f, 50.f);
-	mPlayerAircraft->attachChild(std::move(rightEscort));
 }
